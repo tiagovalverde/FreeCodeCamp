242c250333b3c031247cc9333a5ae41080861893
tiagovalverde <tiago.c.v17@gmail.com>
2017-07-29 12:23:20 -0400

    Equal button operation using eval

--- javascript-calculator/js/index.js
+++ javascript-calculator/js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var input = ""; //used everytime one btn is pressed
   var result = "0"; //used when pressed equals
   var history = "0"; //historic of entire operation
+  var answer = "";
   var MAX_LENGTH = 9; //max digits allowed in screen
   var MAX_LENGTH_HISTORY = 22;
   
@@ -17,18 +18,14 @@ $(document).ready(function() {
       //check if is digit
       if(!isNaN(input)){
 
-          if(checkZero()){
-            //set
-            setResult(input);
-            setHistory(input);
-          }else if(checkSign()){
-             setResult(input);
-             concHistory(input);
-          }else{
-            concResult(input);
-            concHistory(input);
+          if(answer === ""){
+              isDigit(input);  
+          }else if(!isNaN(answer)){
+              var curr_input = input;
+              reset_all();
+              setResult(curr_input);
+              setHistory(curr_input);
           }
-          
           
 
       //check if is dot
@@ -37,42 +34,69 @@ $(document).ready(function() {
          checkDotExists();
 
 
-      }else if(input === "/" || input === "x" || input === "+" || input === "-"){
+      }else if(input === "/" || input === "*" || input === "+" || input === "-"){
 
-        // check is sign already in result
-        if(!checkSign()){
-                  //set result to the sign only
-        //add sign to history
-          concHistory(input);
-          setResult(input);
-        }else if(checkSignHistory()){
-          setResult(input);
+        if(checkZero()){ //avoids sign to be the first input in chain
+          return;
         }
+
+        if(answer===""){
+            // check is sign already in result
+            if(!checkSign()){
+                      //set result to the sign only
+            //add sign to history
+              concHistory(input);
+              setResult(input);
+            }else if(checkSignHistory()){
+              //setResult(input);
+            }
+
+          }else if (!isNaN(answer)){//
+              setHistory(answer + input);
+              setResult(input);
+              answer = "";
+          }
+
+
+
+        
         
 
       }else if(input === "="){
-        console.log("equal sign");
+          
+            console.log(history);
+            try{
+              answer = eval(history);
+              console.log(answer);
+              console.log(Math.round(answer * 100) / 100);
+              answer = Math.round(answer * 100) / 100;
+              setResult(answer);
+              concHistory("=" + answer);
+              console.log("ANS: " + answer);
+              }catch(err){
+                console.log(err.message + ": " + "Incomplete arithmetic operation.");
+              }
+        
+        
         //execute calculation
         //show result in result
         //show result after history
-
-
-      }else if(input === "ac"){ 
-        console.log("clear all");
+      }else if(input === "ac"){       
         reset_all();
-        
       }else if(input === "ce"){ 
-        console.log("ce input");
-        //result reset only, save history
         
-        history = history.substring(0, history.length - result.length);
+        //result reset only, save history
+        if(history.length - result.length !== 0){
+          history = history.substring(0, history.length - result.length);
+        }else{
+          history = "0";
+        }
+        
         setHistory(history);
         setResult("0");
       }
     
     }else{
-      console.log("max reached");
-
       result = "0";
       history = "LIMIT REACHED";
       setResult(result);
@@ -82,17 +106,30 @@ $(document).ready(function() {
       
   });
 
+  //decisions process is digit is clicked
+  function isDigit(newDigit){
+      if(checkZero()){
+        //set
+        setResult(newDigit);
+        setHistory(newDigit);
+      }else if(checkSign()){
+         setResult(newDigit);
+         concHistory(newDigit);
+      }else{
+        concResult(newDigit);
+        concHistory(newDigit);
+      }
+  }
 
   //check if result only have a operation sign
   function checkSign(){
-    return result.length === 1 && (result === "/" || result === "x" || result === "+" || result === "-");
+    return result.length === 1 && (result === "/" || result === "*" || result === "+" || result === "-");
   }
 
   //check if last character of history is a sign
   function checkSignHistory(){
      var lastChar = history.charAt(history.length-1);
-     console.log(lastChar);
-     return lastChar === "/" || lastChar === "x" || lastChar === "+" ||lastChar === "-";
+     return lastChar === "/" || lastChar === "*" || lastChar === "+" ||lastChar === "-";
   }
 
   //check if str result only contains "0"
@@ -128,6 +165,7 @@ $(document).ready(function() {
     input = "";
     result = "0";
     history = "0";
+    answer = "";
     setResult("0");
     setHistory("0");
   }
