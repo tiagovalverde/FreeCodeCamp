fd46cf7de749138c9483ddc4f345f103858bc401
tiagovalverde <tiago.c.v17@gmail.com>
2017-07-29 16:30:23 -0400

    arithmetic operation fixed. Limit on history field

--- javascript-calculator/js/index.js
+++ javascript-calculator/js/index.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
   
-  var input = ""; //used everytime one btn is pressed
+  var input = ""; //used everytime a button is pressed
   var result = "0"; //used when pressed equals
   var history = "0"; //historic of entire operation
   var answer = "";
   var MAX_LENGTH = 9; //max digits allowed in screen
-  var MAX_LENGTH_HISTORY = 22;
+  var MAX_LENGTH_HISTORY = 18;
   
   //click event adding chars to string
   $('button').click(function(){
     //value of button pressed
     input = $(this).attr("value");
-    
+    //console.log(result.length < MAX_LENGTH);
     //check if max reached
-    if( result.length < MAX_LENGTH || history.length <  MAX_LENGTH_HISTORY  ){
+    if(result.length <= MAX_LENGTH){
       
       //check if is digit
       if(!isNaN(input)){
@@ -27,7 +27,6 @@ $(document).ready(function() {
               setHistory(curr_input);
           }
           
-
       //check if is dot
       }else if(input === "."){
         //check if dot already in result
@@ -64,18 +63,26 @@ $(document).ready(function() {
 
       }else if(input === "="){
           
-            console.log(history);
+            //console.log(history);
             try{
               answer = eval(history);
-              console.log(answer);
-              console.log(Math.round(answer * 100) / 100);
+              //console.log(answer);
               answer = Math.round(answer * 100) / 100;
-              setResult(answer);
-              concHistory("=" + answer);
-              console.log("ANS: " + answer);
-              }catch(err){
-                console.log(err.message + ": " + "Incomplete arithmetic operation.");
+
+              if(answer.toString().length > MAX_LENGTH || 
+                (history + "=" + answer).length >  MAX_LENGTH_HISTORY  ){
+                  //reset limit
+                  limitReached();
+                  console.log("AAAAA");
+                  return;
               }
+
+              setResult(answer +"");
+              concHistory("=" + answer);
+              //console.log("ANS: " + answer);
+            }catch(err){
+              console.log(err.message + ": " + "Incomplete arithmetic operation.");
+            }
         
         
         //execute calculation
@@ -97,16 +104,26 @@ $(document).ready(function() {
       }
     
     }else{
-      result = "0";
-      history = "LIMIT REACHED";
-      setResult(result);
-      setHistory(history);
+      limitReached();
     }
      
       
   });
 
-  //decisions process is digit is clicked
+
+  //helper functions
+
+  //resets all fields if limit reached
+  function limitReached(){
+      result = "0";
+      history = "LIMIT REACHED";
+      answer = "";
+      setResult(result);
+      setHistory(history);
+  }
+
+
+  //process if digit is clicked
   function isDigit(newDigit){
       if(checkZero()){
         //set
@@ -121,7 +138,7 @@ $(document).ready(function() {
       }
   }
 
-  //check if result only have a operation sign
+  //check if result field only have a operation sign
   function checkSign(){
     return result.length === 1 && (result === "/" || result === "*" || result === "+" || result === "-");
   }
@@ -132,12 +149,12 @@ $(document).ready(function() {
      return lastChar === "/" || lastChar === "*" || lastChar === "+" ||lastChar === "-";
   }
 
-  //check if str result only contains "0"
+  //check if result field only contains "0"
   function checkZero(){
     return result.length === 1 && result.charAt(0) === "0";
   }
 
-
+  //check if dot was already inserted
   function checkDotExists(){
     //looks for dot in result
     //yes does nothing & exit
@@ -148,19 +165,20 @@ $(document).ready(function() {
     }
   }
 
+  //concatenates the new input to history field
   function concHistory(newInput){
     history += newInput;
     setHistory(history);
-
-
   }
-
+  
+  //concatenates the new input to result field 
   function concResult(newInput){
        result += newInput;
        setResult(result);
    
   }
   
+  //reset all fields - AC button
   function reset_all(){
     input = "";
     result = "0";
