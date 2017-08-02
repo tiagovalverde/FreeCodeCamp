3310bf0bc6d84f41e11d27fd6922dd98302a1aaf
tiagovalverde <tiago.c.v17@gmail.com>
2017-08-02 00:20:19 -0400

    Testing different slider to setup time

--- pomodoro-clock/js/index.js
+++ pomodoro-clock/js/index.js
@@ -2,7 +2,35 @@ $(document).ready(function() {
   
  $('.splus').click(function(){
  	console.log("splus");
+
+
  });
+
+ var rangeSlider = function(){
+  var slider = $('.range-slider'),
+      range = $('.range-slider__range'),
+      value = $('.range-slider__value');
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function(){
+      $(this).next(value).html(this.value);
+    });
+  });
+};
+
+rangeSlider();
+
 
 
 });
+
+
+// Without JQuery
+// With JQuery
+// Without JQuery
