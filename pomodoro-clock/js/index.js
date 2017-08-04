4bf192b07b786caf9434aea9a61ee190395bd0c7
tiagovalverde <tiago.c.v17@gmail.com>
2017-08-04 00:15:14 -0400

    Countdown timer implementation

--- pomodoro-clock/js/index.js
+++ pomodoro-clock/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   
 
+ //sliders effects
  var rangeSlider = function(){
   var slider = $('.range-slider'),
       range = $('.range-slider__range'),
@@ -15,16 +16,89 @@ $(document).ready(function() {
 
     range.on('input', function(){
       $(this).next(value).html(this.value);
+
+     //console.log($(this).attr('id'));
+     if($(this).attr('id') === 'sliderSession'){
+      		
+      		$('#countdown_display .minutes').html(this.value);
+     }
+
+      
     });
   });
 };
 
 rangeSlider();
 
- $('#btn-play').click(function(){
- 	console.log($('.range-slider__value').first().text());
+//
 
- });
+
+ // Click events
+ //$('#btn-play').click(function(){
+ //	console.log($('.range-slider__value').first().text());
+ 	
+
+ //});
+
+
+/*
+document.querySelector('#btn-play').addEventListener('click', function () {
+       
+});
+
+document.querySelector('#btn-pause').addEventListener('click', function () {
+
+});
+
+document.querySelector('#btn-reset').addEventListener('click', function () {
+
+});
+
+*/
+
+
+$(".seconds").on('DOMSubtreeModified', function () {
+    //alert("Span HTML is now " + $(this).html());
+    if($(this).html() === '0' && $('.minutes').html() === '0'){
+    	console.log($('.seconds').html());
+    	console.log("Finished"); //start next countdown
+    	var slider_timeSeconds = parseInt($('.range-slider__value').second().text()) * 60 ; 
+    	timer.start({precision: 'seconds',countdown: true, startValues: {seconds: slider_timeSeconds}});
+    }
+});
+
+
+//easytimer call
+var timer = new Timer();
+
+
+$('.buttons #btn-play').click(function () {
+	var slider_timeSeconds = parseInt($('.range-slider__value').first().text()) * 60 ; 
+	console.log(slider_timeSeconds);
+    timer.start({precision: 'seconds',countdown: true, startValues: {seconds: slider_timeSeconds}});
+});
+
+$('.buttons #btn-pause').click(function () {
+    timer.pause();
+});
+$('.buttons #btn-reset').click(function () {
+    timer.stop();
+    $('#countdown_display .minutes').html($('.range-slider__value').first().text());
+     $('#countdown_display .seconds').html("00");
+});
+
+
+timer.addEventListener('secondsUpdated', function (e) {
+	 $('#countdown_display .minutes').html(timer.getTimeValues().minutes);
+     $('#countdown_display .seconds').html(timer.getTimeValues().seconds);
+
+});
+    
+timer.addEventListener('started', function (e) {
+    	 $('#countdown_display .minutes').html(timer.getTimeValues().minutes);
+     $('#countdown_display .seconds').html(timer.getTimeValues().seconds);
+});
+
 
 
 
