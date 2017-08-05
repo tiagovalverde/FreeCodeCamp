c7e87a16e676c1194a7d4b47374d582973deec9e
tiagovalverde <tiago.c.v17@gmail.com>
2017-08-04 23:41:09 -0400

    Countdown timer fully configured

--- pomodoro-clock/js/index.js
+++ pomodoro-clock/js/index.js
@@ -56,14 +56,25 @@ document.querySelector('#btn-reset').addEventListener('click', function () {
 
 */
 
-
+//every time there is a change in seconds element
 $(".seconds").on('DOMSubtreeModified', function () {
-    //alert("Span HTML is now " + $(this).html());
+    //check if finished the countdown
     if($(this).html() === '0' && $('.minutes').html() === '0'){
-    	console.log($('.seconds').html());
-    	console.log("Finished"); //start next countdown
-    	var slider_timeSeconds = parseInt($('.range-slider__value').second().text()) * 60 ; 
-    	timer.start({precision: 'seconds',countdown: true, startValues: {seconds: slider_timeSeconds}});
+
+    	if($('#countdown_title').html() === 'Session'){
+    		//starts the break countdown
+    		timer.stop();
+    		var slider_timeSeconds = parseInt($('.range-slider__value').eq(1).text()) * 60 ; 
+            timer.start({precision: 'seconds',countdown: true, startValues: {seconds: slider_timeSeconds}});
+            $('#countdown_title').html('Break');
+
+    	}else{
+    		//starts the Session countdown again
+    		timer.stop();
+    		var slider_timeSeconds = parseInt($('.range-slider__value').eq(0).text()) * 60 ; 
+            timer.start({precision: 'seconds',countdown: true, startValues: {seconds: slider_timeSeconds}});
+            $('#countdown_title').html('Session');
+    	}
     }
 });
 
@@ -71,11 +82,10 @@ $(".seconds").on('DOMSubtreeModified', function () {
 //easytimer call
 var timer = new Timer();
 
-
 $('.buttons #btn-play').click(function () {
 	var slider_timeSeconds = parseInt($('.range-slider__value').first().text()) * 60 ; 
-	console.log(slider_timeSeconds);
     timer.start({precision: 'seconds',countdown: true, startValues: {seconds: slider_timeSeconds}});
+    disableSlider();
 });
 
 $('.buttons #btn-pause').click(function () {
@@ -83,29 +93,35 @@ $('.buttons #btn-pause').click(function () {
 });
 $('.buttons #btn-reset').click(function () {
     timer.stop();
-    $('#countdown_display .minutes').html($('.range-slider__value').first().text());
-     $('#countdown_display .seconds').html("00");
+    updateCounterPerSecond( $('.range-slider__value').first().text(), '00');
+    enableSlider();
 });
 
 
 timer.addEventListener('secondsUpdated', function (e) {
-	 $('#countdown_display .minutes').html(timer.getTimeValues().minutes);
-     $('#countdown_display .seconds').html(timer.getTimeValues().seconds);
-
+	updateCounterPerSecond( timer.getTimeValues().minutes, timer.getTimeValues().seconds);
 });
     
 timer.addEventListener('started', function (e) {
-    	 $('#countdown_display .minutes').html(timer.getTimeValues().minutes);
-     $('#countdown_display .seconds').html(timer.getTimeValues().seconds);
+	 updateCounterPerSecond( timer.getTimeValues().minutes, timer.getTimeValues().seconds);
 });
 
+function disableSlider(){
+	document.getElementById("sliderBreak").disabled = true;
+    document.getElementById("sliderSession").disabled = true;
+}
 
+function enableSlider(){
+	document.getElementById("sliderBreak").disabled = false;
+    document.getElementById("sliderSession").disabled = false;
+}
+
+function updateCounterPerSecond( min, sec){
+	 $('#countdown_display .minutes').html(min);
+     $('#countdown_display .seconds').html(sec);
+}
 
 
 
 });
 
-
-// Without JQuery
-// With JQuery
-// Without JQuery
