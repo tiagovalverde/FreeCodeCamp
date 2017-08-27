31e00718bb53019890bf40a1b429acc292f66f0a
tiagovalverde <tiago.c.v17@gmail.com>
2017-08-27 16:32:29 -0400

    built board and event listeners added

--- tictactoe-game/js/index.js
+++ tictactoe-game/js/index.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-  
+	
+	var btn_reset = document.getElementById('btn-reset');
+    btn_reset.onclick = function() {resetGame()};
+
+	function resetGame(){
+		console.log('hide board, clean record, ask X or O, showboard');
+	}
+
+
+var board_pos = undefined;
+
+	const board_list = document.querySelectorAll('.board-block');
+
+    board_list.forEach(
+    	key => addEventListener("click", once, false)
+
+    );
+
+
+
+
+
+    function once(element) {
+	    
+	    console.log( element.target.getAttribute('value') );
+	  }
+
 });
+
+
+
+
