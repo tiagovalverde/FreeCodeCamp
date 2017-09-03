07a6f98bd58f196c63ee94026e96683eca6ec66f
tiagovalverde <tiago.c.v17@gmail.com>
2017-09-03 11:39:45 -0400

    OnClick events and fill board

--- tictactoe-game/js/index.js
+++ tictactoe-game/js/index.js
@@ -1,8 +1,11 @@
-    //global vars
-    var player = "";
-    var player_record = 0;
-    var com = "";
-    var com_record = 0;
+var game_prop = {
+    player: "",
+    player_record: 0,
+    com: "",
+    com_record: 0,
+    currentPlayer: "",
+    moves: 1
+ };
 
 
     //reset game
@@ -11,59 +14,49 @@
 
 	function resetGame(){
 		//hide board, clean record, ask X or O, showboard
+		
+		//styling
 		document.querySelector(".start-block").classList.remove("m-fadeOut");
         document.querySelector(".white-block").classList.remove("m-fadeIn");
-        player = com = "";
-        player_record = com_record = 0
+        
+        //set up game properties
+        game_prop.player = com = "";
+        game_prop.player_record = game_prop.com_record = 0;
+        console.log( 'game reset');
 	}
 
-	//board click events
 
-	function addBoardClickEvents(){
-		var board_pos = undefined;
-		const board_list = document.querySelectorAll('.board-block');
-
-	    board_list.forEach(
-	    	key => addEventListener("click", fillBlock, false)
-
-	    );
-
-
-	    function fillBlock(element) {
-		    
-		    console.log( element.target.getAttribute('value') );
-		}
-	}
-
-    
+	
 
 	//starting game events
 	const startButtons = document.querySelectorAll('.btn-secondary');
     console.log(startButtons);
 
 	startButtons.forEach(
-		key => addEventListener('click', selectPlayerChar)
+		key => addEventListener('click', selectPlayerChar, false)		
     );
 
 	
-
 	function selectPlayerChar(element){
 		//define chars for players
 		if(element.target.getAttribute('id') === "start-btn"){
-			console.log("sss");
-			player = element.target.getAttribute('data-player');
-            com = element.target.getAttribute('data-com');
+			console.log("select char & start");
+			game_prop.player = element.target.getAttribute('data-player');
+            game_prop.com = element.target.getAttribute('data-com');
+            currentPlayer = game_prop.player; // switch each game (depending on number games realized)
             //hide starting block ^& show block
             document.querySelector(".start-block").className += ' m-fadeOut';
             document.querySelector(".white-block").className += ' m-fadeIn';
 		}
-		
-        
-        
 	}
 
 
-
+	//fill Board
+	function icon(block_id){
+		
+        document.getElementById(block_id).removeAttribute("onClick");
+        document.getElementById(block_id).innerText = game_prop.player;
+	}
 
 
 
