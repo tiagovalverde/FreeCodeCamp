173a3a3ba75e9444e0b748d8868700d115c17355
Tiago Valverde <tiago.c.v17@gmail.com>
2018-01-19 23:08:31 -0500

    check board for tie, disable onclick events

--- tictactoe-game/js/index.js
+++ tictactoe-game/js/index.js
@@ -23,7 +23,6 @@ startButtons.forEach(
 function selectPlayerChar(element){
 	//define chars for players
 	if(element.target.getAttribute('id') === "start-btn"){
-		console.log("select char & start");
         setupGameProp(element);
         showGameBoard();
         setupRecordDataAttr(element); 
@@ -40,7 +39,6 @@ function resetGame(){
 	hideGameBoard();
     resetGameProp();
     resetBoard();
-    console.log( 'game reset');
 }
 
 //fill Board
@@ -70,17 +68,19 @@ function updateBoardBlock(block_id){
 	//fill block with current player char
     document.getElementById(block_id).innerText = game_prop.crrt_plr;
     game_prop.board[block_id] = game_prop.crrt_plr;
-    console.log(game_prop.board);
 }
 
 
-
+	//increments victory on winner and highlights board blocks
 	function showWinner(blocksWinID, winner){
 
-		console.log("showWinner ----------");
-		console.log(winner);
-
+		//disable onclick events on blocks
+		//highlight board
 		boardBlocks.forEach(block => {
+			
+			document.getElementById(block.id).onclick = null;
+
+			
 			if(block.id == blocksWinID[0] || block.id == blocksWinID[1] || block.id == blocksWinID[2]){
 				document.getElementById(blocksWinID[0]).classList.add("win");
 				document.getElementById(blocksWinID[1]).classList.add("win");
@@ -91,33 +91,59 @@ function updateBoardBlock(block_id){
 		setTimeout(updateRecord, 1000);
 		clearInterval(updateRecord);
 
+		setTimeout(resetBoard, 1500);
+		clearInterval(resetBoard);
+
 		function updateRecord(){
 		    if(winner === document.querySelectorAll(".record")[0].getAttribute('data-player')){
-				var new_record = game_prop.plr_rcrd+1;
-				document.querySelectorAll(".record span")[0].innerText = new_record;
+				game_prop.plr_rcrd += 1;
+				document.querySelectorAll(".record span")[0].innerText = game_prop.plr_rcrd;
 		    }else{
-				var new_record = game_prop.com_rcrd+1;
-				document.querySelectorAll(".record span")[1].innerText = new_record;
+				game_prop.com_rcrd += 1;
+				document.querySelectorAll(".record span")[1].innerText = game_prop.com_rcrd;
 			}
 		}
 	}
 
-	function checkStateGame(currentPlayer){
 
-		var temp = currentPlayer + "WINSSS!";
+	//highlight all blocks due to tie
+	function highlightBlocksTie(){
+		boardBlocks.forEach(block => {
+			block.classList.add("tie");
+		});
+	}
+
+	//check if board is full
+	function checkIfBoardFull(){
+
+		isFull = 0;
+		boardBlocks.forEach(block => {
+
+			if(block.innerText != ''){
+				isFull++;
+			}
+
+			if(isFull === 9){
+				//tie
+				highlightBlocksTie();
+				setTimeout(resetBoard, 1500);
+		        clearInterval(resetBoard);
+			}
+		});
+	}
+
+	function checkStateGame(currentPlayer){
 
 		if(game_prop.board[0] === currentPlayer){
 
 			if(game_prop.board[1] === currentPlayer && game_prop.board[2] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [0,1,2];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}else if(game_prop.board[3] === currentPlayer && game_prop.board[6] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [0,3,6];
 				showWinner(winnerBlocksPos,currentPlayer );
+
 			}else if(game_prop.board[4] === currentPlayer && game_prop.board[8] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [0,4,8];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}
@@ -126,7 +152,6 @@ function updateBoardBlock(block_id){
 		}else if(game_prop.board[1] === currentPlayer){
 
 			if(game_prop.board[4] === currentPlayer && game_prop.board[7] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [1,4,7];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}
@@ -134,11 +159,9 @@ function updateBoardBlock(block_id){
 		}else if(game_prop.board[2] === currentPlayer){
 
 			if(game_prop.board[5] === currentPlayer && game_prop.board[8] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [2,5,8];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}else if(game_prop.board[4] === currentPlayer && game_prop.board[6] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [2,4,6];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}
@@ -146,7 +169,6 @@ function updateBoardBlock(block_id){
 		}else if(game_prop.board[3] === currentPlayer){
 			
 			if(game_prop.board[4] === currentPlayer && game_prop.board[5] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [3,4,5];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}
@@ -154,19 +176,17 @@ function updateBoardBlock(block_id){
 		}else if(game_prop.board[6] === currentPlayer){
 			
 			if(game_prop.board[7] === currentPlayer && game_prop.board[8] === currentPlayer){
-				console.log(temp);
 				const winnerBlocksPos = [6,7,8];
 				showWinner(winnerBlocksPos,currentPlayer );
 			}
 		}
+
+		checkIfBoardFull();
+
 	}
 
 
 // Helpers
-function gameOver(){
-
-}
-
 //selectPlayerChar()
 function showGameBoard(){
     document.querySelector(".start-block").className += ' m-fadeOut';
@@ -201,12 +221,19 @@ function resetGameProp(){
 	game_prop.game_over = 0;
 }
 
+function resetBoardProp(){
+	game_prop.moves = 0;
+	game_prop.board = [0,1,2,3,4,5,6,7,8];
+}
+
 function resetBoard(){
 	//reset game properties (elements)
     boardBlocks.forEach(block => {
     	block.innerHTML = "";
     	block.onclick = function() {fillBlock(this.id)};
-    	block.classList.remove("win");
+		block.classList.remove("win");
+		block.classList.remove("tie");
+		resetBoardProp();
     	//add onclick attribute
     });
 }
