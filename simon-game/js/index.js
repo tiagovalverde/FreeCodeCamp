14401e22c6bfcda1dfb5acaace0d0e0320b0ff4c
Tiago Valverde <tiago.c.v17@gmail.com>
2018-01-31 07:41:59 -0500

    start and strict button onclick events, reset game object

--- simon-game/js/index.js
+++ simon-game/js/index.js
@@ -10,21 +10,58 @@ SIMON_GAME = {
 };
 
 //controls events
-var btn_power_switch = document.getElementById('power-switch');
+var power_switch_btn = document.getElementById('power-switch');
+var start_btn = document.getElementById('start');
+var strict_btn = document.getElementById('strict');
+var strict_indicator = document.getElementById('strict-indicator');
+var counter_txt = document.getElementById('counter');
 
-btn_power_switch.onclick = function() {onPowerSwitchClick()};
 
+power_switch_btn.onclick = function() {onPowerSwitchClick()};
 
 //click events
 function onPowerSwitchClick(){
     console.log("power switch clicked");
-    console.log(btn_power_switch);
+
     if(SIMON_GAME.is_off){
-        btn_power_switch.classList.add('on');
+        power_switch_btn.classList.add('on');
+        counter_txt.classList.remove('led-off');
         SIMON_GAME.is_off = false;
+        start_btn.onclick = function() {onStartClick()};
+        strict_btn.onclick = function() {onStrictClick()};
     }else{
-        btn_power_switch.classList.remove('on');
-        SIMON_GAME.is_off = true;
+        power_switch_btn.classList.remove('on');
+        counter_txt.classList.add('led-off');
+        strict_indicator.classList.remove("full-red");
+        resetGameOnject();
+        //disable color onclick ???
+        //reset counter to "--"
+        start_btn.onclick = null;
+        strict_btn.onclick = null;
     }
-   
+}
+
+function onStartClick(){
+    console.log("start clicked");
+}
+
+function onStrictClick() {
+    if(SIMON_GAME.is_strict_mode){
+        SIMON_GAME.is_strict_mode = false;
+        strict_indicator.classList.remove('full-red');
+    }else{
+        SIMON_GAME.is_strict_mode = true;
+        strict_indicator.classList.add('full-red');
+    }
+    //console.log("srtict: " + SIMON_GAME.is_strict_mode);
+}
+
+function resetGameOnject() {
+    SIMON_GAME.counter = 0;
+    SIMON_GAME.sequence = [];
+    SIMON_GAME.next_to_validate = 0;
+    SIMON_GAME.is_off = true;
+    SIMON_GAME.is_playing_sequence = false;
+    SIMON_GAME.is_strict_mode = false;
+    MAX_NUM_SEQUENCE = 20;
 }
