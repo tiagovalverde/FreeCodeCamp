ac4eecddff04ae92079e34435d8c431e9931e3e7
Tiago Valverde <tiago.c.v17@gmail.com>
2018-02-08 15:19:19 -0500

    restart game functionality finished

--- simon-game/js/index.js
+++ simon-game/js/index.js
@@ -6,7 +6,7 @@ SIMON_GAME = {
 	is_off: true,
 	is_playing_sequence: false,
 	is_strict_mode: false,
-	MAX_NUM_SEQUENCE: 20,
+	MAX_NUM_SEQUENCE: 10,
 	colors: ['blue', 'red', 'green', 'yellow'],
 	interrupt: false, //flag var for start new game (promise handler)
 };
@@ -17,35 +17,65 @@ var start_btn = document.getElementById('start');
 var strict_btn = document.getElementById('strict');
 var strict_indicator = document.getElementById('strict-indicator');
 var counter_txt = document.getElementById('counter');
+//btn labels
+var labels = document.querySelectorAll('.control-desc');
+var start_label = labels[0];
+console.log(start_label.innerText);
 
-power_switch_btn.onclick = function() {
+power_switch_btn.onclick = function () {
 	onPowerSwitchClick();
 };
 
 //click events
 function onPowerSwitchClick() {
-	console.log('power switch clicked');
-
 	if (SIMON_GAME.is_off) {
 		power_switch_btn.classList.add('on');
 		counter_txt.classList.remove('led-off');
 		SIMON_GAME.is_off = false;
-		start_btn.onclick = function() {
-			onStartClick();
-		};
-		strict_btn.onclick = function() {
-			onStrictClick();
-		};
+		enableStartButton();
+		enableStrictButton();
 	} else {
 		power_switch_btn.classList.remove('on');
 		counter_txt.classList.add('led-off');
 		strict_indicator.classList.remove('full-red');
 		resetGameOnject();
-		//disable color onclick ???
-		//reset counter to "--"
-		start_btn.onclick = null;
-		strict_btn.onclick = null;
+		disableStartButton();
+		disableStrictButton();
 	}
+}
+
+function enableStrictButton() {
+	strict_btn.classList.add('clickable');
+	strict_btn.onclick = function () {
+		onStrictClick();
+	};
+}
+
+function enableStartButton() {
+	console.log(SIMON_GAME.sequence.length);
+	if (SIMON_GAME.sequence.length !== 0) {
+		start_label.innerHTML = 'RESTART';
+	} else {
+		start_label.innerHTML = 'START';
+	}
+
+	start_label.classList.remove('line-through');
+	start_btn.classList.add('clickable');
+	start_btn.onclick = function () {
+		onStartClick();
+	};
+}
+
+function disableStrictButton() {
+	strict_btn.classList.remove('clickable');
+	strict_btn.onclick = null;
+}
+
+function disableStartButton() {
+	start_label.classList.add('line-through');
+	start_btn.classList.remove('clickable');
+	start_btn.onclick = null;
+
 }
 
 async function onStartClick() {
@@ -55,16 +85,22 @@ async function onStartClick() {
 		resetGameOnject(); //if is in progress
 		removeColorsOnClick();
 		resetGameDesign();
+		start_label.innerHTML = 'START';
 	}
 	var result = await flashCounterLed();
-	console.log(result);
+	//console.log(result);
 	playSequence();
 }
 
 async function replaySequence() {
+	//disable start button
+	disableStartButton();
 	counter_txt.innerText = SIMON_GAME.counter;
 	SIMON_GAME.is_playing_sequence = true;
+
 	var result = await displaySequence();
+	//enable start button to be Restart
+	enableStartButton();
 	SIMON_GAME.is_playing_sequence = false;
 	if (result === 'interrupted') {
 		return;
@@ -78,23 +114,22 @@ async function playSequence() {
 	SIMON_GAME.sequence.push(getNextColorRand());
 	counter_txt.innerText = SIMON_GAME.counter;
 	console.log(SIMON_GAME.sequence);
+	disableStartButton();
 	var result = await displaySequence();
-	console.log(result);
+	enableStartButton();
 	//player turn
 	SIMON_GAME.is_playing_sequence = false;
 	if (result === 'interrupted') {
 		return;
 	}
-	//add onCLick events on colors
 	setupColorsOnClick();
-	//finish and wait for user clicks
 }
 
 function setupColorsOnClick() {
 	var color_blocks = document.querySelectorAll('.color-blocks');
 
 	color_blocks.forEach(color => {
-		color.onclick = function() {
+		color.onclick = function () {
 			onClickColor(color.id);
 		};
 
@@ -116,6 +151,7 @@ function removeColorsOnClick() {
 function addLightColor() {
 	this.classList.add(this.id + '-light');
 }
+
 function removeLightColor() {
 	this.classList.remove(this.id + '-light');
 }
@@ -123,24 +159,20 @@ function removeLightColor() {
 async function onClickColor(color_id) {
 	//check if value on index match user id
 	if (color_id === SIMON_GAME.sequence[SIMON_GAME.user_click_counter]) {
-		console.log('match');
+		//console.log('match');
 		if (SIMON_GAME.user_click_counter === SIMON_GAME.sequence.length - 1) {
-			console.log('finished guessing sequence');
 			removeColorsOnClick();
 			SIMON_GAME.user_click_counter = 0;
 			playSequence();
 		} else {
-			console.log('continue guessing sequence');
 			SIMON_GAME.user_click_counter++;
 		}
 	} else {
-		console.log('fail');
 		//if no match
 		if (SIMON_GAME.is_strict_mode) {
 			//reset and start sequence over
 		} else {
 			//same sequence is played again (strict false)
-
 			counter_txt.innerText = 'NO';
 			var result = await flashCounterLed();
 			SIMON_GAME.user_click_counter = 0;
@@ -155,7 +187,7 @@ function displaySequence() {
 
 	return new Promise(resolve => {
 		let in_out = 0;
-		SIMON_GAME.sequence.map(function(color, index) {
+		SIMON_GAME.sequence.map(function (color, index) {
 			var lighter = setTimeout(() => {
 				document.getElementById(color).classList.add(color + '-light');
 			}, 500 + index * 1000);
@@ -193,7 +225,6 @@ function onStrictClick() {
 		SIMON_GAME.is_strict_mode = true;
 		strict_indicator.classList.add('full-red');
 	}
-	//console.log("srtict: " + SIMON_GAME.is_strict_mode);
 }
 
 function flashCounterLed() {
@@ -201,6 +232,7 @@ function flashCounterLed() {
 	function addLed() {
 		counter_txt.classList.add('led-off');
 	}
+
 	function removeLed() {
 		counter_txt.classList.remove('led-off');
 	}
@@ -211,7 +243,7 @@ function flashCounterLed() {
 	//switch between add and remove functions with timers
 	//javascript does not keep running unitl resolve returned
 	return new Promise(resolve => {
-		flashingArray.map(function(fun, index) {
+		flashingArray.map(function (fun, index) {
 			setTimeout(() => {
 				fun();
 
